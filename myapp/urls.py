ad3bfb474e1151a3db2cd338eefda682ca0d8eca
kabo_g <kabogalefete@gmail.com>
2022-11-21 12:57:10 +0800

    added url routing for the application

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+from . views import SignatureView
+urlpatterns = [
+    path("", SignatureView.as_view(), name = "home"),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
+
 
 # Create your views here.
+
+class SignatureView(View):
+
+    pass

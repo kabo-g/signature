94197d2d7511b20b4941b7acd8bdc2e805351d31
kabo_g <kabogalefete@gmail.com>
2022-11-21 14:57:48 +0800

    added html, frontend code

--- dsignature/settings.py
+++ dsignature/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'dsignature.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_MEDIA = 'static'
+
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import ServerRoom
 # Register your models here.
+
+admin.site.register(ServerRoom)

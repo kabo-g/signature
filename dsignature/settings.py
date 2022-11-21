db793ed810ef2add66c18ee4219cf7817a66482d
kabo_g <kabogalefete@gmail.com>
2022-11-21 13:51:23 +0800

    create models

--- dsignature/settings.py
+++ dsignature/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy-forms'
+    'crispy_forms',
     'jsignature',
     'myapp.apps.MyappConfig',
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from jsignature.fields import JSignatureField
 
 # Create your models here.
+
+class ServerRoom(models.Model):
+
+    first_name = models.CharField(max_length = 200, blank = True, null = True)
+    last_name = models.CharField(max_length = 200, blank = True, null = True)
+    phone_no = models.CharField(max_length = 8, blank = True, null = True)
+    company = models.CharField(max_length = 200, blank = True, null = True)
+    date = models.DateField(null=True, blank = True)
+    time_in = models.TimeField(null = True, blank = True)
+    time_out = models.TimeField(null = True, blank = True)
+    signature = JSignatureField(null = True, blank = True)
+
+
+    def __str__(self):
+
+        return self.first_name
+
+    class Meta:
+        pass
